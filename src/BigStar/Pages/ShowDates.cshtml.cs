b8621b0427b8b77baed55f0c69f34d8622599f17
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-24 07:16:30 +0000

    add AllDatesInMonth method

--- src/BigStar/Pages/ShowDates.cshtml.cs
+++ src/BigStar/Pages/ShowDates.cshtml.cs
@@ -3,10 +3,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MyApp.Namespace
 {
-    public class ShowDatesModel : PageModel
+  public class ShowDatesModel : PageModel
+  {
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
+
+    public IEnumerable<DateOnly> AllDatesInMonth(int year, int month)
+    {
+      int days = DateTime.DaysInMonth(year, month);
+      for (int day = 1; day <= days; day++)
+      {
+        yield return new DateOnly(year, month, day);
+      }
+
+    }
+  }
 }
