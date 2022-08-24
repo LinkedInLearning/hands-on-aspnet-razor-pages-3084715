58003d83736f41c91881850de6f929cb1d559ba9
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-24 04:59:15 +0000

    Add more pages

---
+++ src/BigStar/Pages/ShowDates.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ShowDatesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/BigStar/Pages/Store/Sale.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class SaleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
