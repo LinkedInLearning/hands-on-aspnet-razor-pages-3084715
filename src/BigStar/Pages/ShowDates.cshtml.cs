ec826256d1cad79ec82b83e0f6c2d7da522aac75
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-25 23:46:24 +0000

    Finished example

--- src/BigStar/Pages/ShowDates.cshtml.cs
+++ src/BigStar/Pages/ShowDates.cshtml.cs
@@ -5,10 +5,19 @@ namespace MyApp.Namespace
 {
   public class ShowDatesModel : PageModel
   {
+    public IEnumerable<DateOnly> SaleDates { get; set; }
     public void OnGet()
     {
+      SaleDates = GetSaleDates(DateOnly.FromDateTime(DateTime.Now));
     }
+    private IEnumerable<DateOnly> GetSaleDates(DateOnly startingDate)
+    {
+      var allDays = AllDatesInMonth(startingDate.Year, startingDate.Month);
+      var SaleDays = allDays.Where(x => x.DayOfWeek == DayOfWeek.Monday
+                                    || x.DayOfWeek == DayOfWeek.Wednesday);
+      return SaleDays;
 
+    }
     public IEnumerable<DateOnly> AllDatesInMonth(int year, int month)
     {
       int days = DateTime.DaysInMonth(year, month);
