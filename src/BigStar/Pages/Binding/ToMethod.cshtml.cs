2c89ac6a3b4b2a660b8d5a4b58d94133b381d9ed
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-26 01:02:16 +0000

    Add example pages

---
+++ src/BigStar/Pages/Binding/ToMethod.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigStar.Pages.Binding
+{
+    public class ToMethodModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/BigStar/Pages/Binding/ToQueryString.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigStar.Pages.Binding
+{
+    public class ToQueryStringModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
