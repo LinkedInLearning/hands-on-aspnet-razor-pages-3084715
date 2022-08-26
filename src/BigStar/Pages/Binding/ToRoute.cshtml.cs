014faf5d9d0d8c3ee30b26f581b26ba5388ec0f3
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-26 08:09:42 +0000

    Add Example Code

--- src/BigStar/Pages/Binding/ToRoute.cshtml.cs
+++ src/BigStar/Pages/Binding/ToRoute.cshtml.cs
@@ -3,13 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BigStar.Pages.Binding
 {
-	[BindProperties(SupportsGet = true)]
-	public class ToRouteModel : PageModel
-	{
-		public string CardName { get; set; }
-		public decimal? OriginalPrice { get; set; }
-		public void OnGet()
-		{
-		}
-	}
+  [BindProperties(SupportsGet = true)]
+  public class ToRouteModel : PageModel
+  {
+    public string CardName { get; set; }
+    public decimal OriginalPrice { get; set; }
+    public void OnGet()
+    {
+    }
+  }
 }
