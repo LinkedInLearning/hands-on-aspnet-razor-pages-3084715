eae2f6da050cf5155f3e7f1fcf76945f230c9a76
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-28 02:03:06 +0000

    Remove Model Binding properties

--- src/BigStar/Pages/Binding/ToMethod.cshtml.cs
+++ src/BigStar/Pages/Binding/ToMethod.cshtml.cs
@@ -4,13 +4,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace BigStar.Pages.Binding
 {
 
-	[BindProperties(SupportsGet = true)]
-	public class ToMethodModel : PageModel
-	{
-		public string CardName { get; set; }
-		public decimal? OriginalPrice { get; set; }
-		public void OnGet()
-		{
-		}
-	}
+  [BindProperties(SupportsGet = true)]
+  public class ToMethodModel : PageModel
+  {
+
+    public void OnGet()
+    {
+    }
+  }
 }
