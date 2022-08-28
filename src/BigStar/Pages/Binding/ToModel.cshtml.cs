7c35bb078892cfc9791cd6589fc3268add7f84f4
Walt Ritscher - Instructor Account <staffauthor@outlook.com>
2022-08-28 01:30:59 +0000

    Binding to custom class finished

--- src/BigStar/Pages/Binding/ToModel.cshtml.cs
+++ src/BigStar/Pages/Binding/ToModel.cshtml.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BigStar.Pages.Binding
 {
-	[BindProperties(SupportsGet = true)]
-	public class ToModelModel : PageModel
-	{
-		public string CardName { get; set; }
-		public decimal? OriginalPrice { get; set; }
-		public void OnGet()
-		{
-		}
-	}
+  [BindProperties(SupportsGet = true)]
+  public class ToModelModel : PageModel
+  {
+    public string CardName { get; set; }
+    public decimal? OriginalPrice { get; set; }
+    public BigStar.Models.CollectibleCard CurrentCard { get; set; }
+    public void OnGet()
+    {
+    }
+  }
 }
