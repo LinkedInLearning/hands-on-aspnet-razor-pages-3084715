0734d4222667396364f13277229f6975d3ed28a7
Walt Ritscher <waltritscher@outlook.com>
2022-08-29 11:34:58 -0700

    Add the PriceYourCards page

---
+++ src/BigStar/Pages/Forms/PriceYourCards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BigStar.Pages.Forms
+{
+    public class PriceYourCardsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
