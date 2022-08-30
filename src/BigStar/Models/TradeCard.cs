a173d5d032c62fcc9928e15fc605b7efbd11ce98
Walt Ritscher <waltritscher@outlook.com>
2022-08-29 22:11:42 -0700

    Add annotations

--- src/BigStar/Models/TradeCard.cs
+++ src/BigStar/Models/TradeCard.cs
@@ -3,7 +3,9 @@ namespace BigStar.Models
 {
 	public class TradeCard
 	{
+		[Display(Name = "How many cards?", Prompt = "Number of cards to sell")]
 		public int CardCount { get; set; } = 1;
+		[Display(Name = "Card Name", Prompt = "Add the card name")]
 		public string CardName { get; set; }
 		public Condition Condition { get; set; } = Condition.Mint;
 	}
